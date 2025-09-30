853e86c2c719787a4a19286697b23a0ffae2466c
KaiSereni <kaiserenior@gmail.com>
2025-09-30 15:17:31 -0400

    default next stuff

--- README.md
+++ README.md
@@ -8,6 +8,7 @@
 * Create a Firebase project and enable billing
 * Put your project ID in [.firebaserc](./.firebaserc)
 * Put your project name in [package.json](./package.json)
+* Create an app in Firebase project settings and put the config in [firebase.tsx](./src/app/firebase.tsx)
 * Create a venv called `venv` in `./functions`, activate it, then install the requirements.txt
 * Run `npm i` in the root
 * To start the emulator, run `npm run emulate`. To deploy, run `firebase deploy`.
---
+++ src/app/firebase.tsx
@@ -0,0 +1,25 @@
+import { initializeApp } from "firebase/app";
+import { connectAuthEmulator, getAuth, GoogleAuthProvider } from "firebase/auth";
+import { getFunctions, httpsCallable, connectFunctionsEmulator } from "firebase/functions";
+import { connectFirestoreEmulator, getFirestore } from "firebase/firestore";
+
+const firebaseConfig = {} // CONFIG GOES HERE
+
+const app = initializeApp(firebaseConfig);
+
+export const auth = getAuth(app);
+export const googleProvider = new GoogleAuthProvider();
+export const functions = getFunctions(app);
+export const firestore = getFirestore(app);
+
+if (typeof window !== "undefined" && process.env.NODE_ENV == "development") {
+  connectFunctionsEmulator(functions, "localhost", 5001);
+  connectAuthEmulator(auth, "http://localhost:9099");
+  connectFirestoreEmulator(firestore, "localhost", 8080);
+}
+
+export function getSaveUserProfileCallable() {
+  return httpsCallable(functions, "save_user_profile");
+}
+
+
